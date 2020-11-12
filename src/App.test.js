ecf5e647a156b6ec9a562573d9b8451ea867e91a
Jordan <jordanleelong5@gmail.com>
2020-11-11 22:38:33 -0800

    imported enzyme and adapter for TDD

--- src/App.test.js
+++ src/App.test.js
@@ -1,8 +1,19 @@
-import { render, screen } from '@testing-library/react';
 import App from './App';
+import Enzyme, {shallow} from 'enzyme'
+import EnzymeAdapter from '@wojtekmaj/enzyme-adapter-react-17';
 
-test('renders learn react link', () => {
-  render(<App />);
-  const linkElement = screen.getByText(/learn react/i);
-  expect(linkElement).toBeInTheDocument();
+// set up enzymes react adapter
+Enzyme.configure({ adapter: new EnzymeAdapter() }); 
+
+test("component renders without error", () => {
+
+}); 
+
+test("renders button", () => {
+
 });
+
+test("renders counter display", () => {
+
+});
+
