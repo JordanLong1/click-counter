e945f51f2609d4804b2cee9610c3be495ed165e4
Jordan <jordanleelong5@gmail.com>
2020-11-12 22:35:36 -0800

    added button for decrement

--- src/App.js
+++ src/App.js
@@ -1,10 +1,24 @@
-import logo from './logo.svg';
+import React, { useState } from 'react';
 import './App.css';
 
 function App() {
+  const [count, setCount] = React.useState(0)
   return (
-    <div data-test='component-app'>
-      
+    <div data-test="component-app">
+      <h1 data-test="counter-display">The counter is currently: 
+  <span data-test="count">{count}</span>
+       </h1>
+      <button data-test="increment-button"
+      onClick={() => setCount(count + 1)}
+      >
+        Increment Counter
+        </button>
+
+        <button data-test="decrement-counter"
+        onClick={() => setCount(count - 1)}
+        >
+        Decrement Counter
+        </button>
     </div>
   );
 }
