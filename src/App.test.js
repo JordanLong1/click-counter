90b3f9218b7ce19646ce889b17db3e6294f4b81c
Jordan <jordanleelong5@gmail.com>
2020-11-12 22:35:14 -0800

    added tests for decrementing button and on click decrement

--- src/App.test.js
+++ src/App.test.js
@@ -1,3 +1,4 @@
+import React from 'react'
 import App from './App';
 import Enzyme, {shallow} from 'enzyme'
 import EnzymeAdapter from '@wojtekmaj/enzyme-adapter-react-17';
@@ -5,26 +6,68 @@ import EnzymeAdapter from '@wojtekmaj/enzyme-adapter-react-17';
 // set up enzymes react adapter
 Enzyme.configure({ adapter: new EnzymeAdapter() }); 
 
+
+const setup = () => shallow(<App />);
+
+const findByTestAttribute = (wrapper, val) => wrapper.find(`[data-test='${val}']`)
+
 test("component renders without error", () => {
-  const wrapper = shallow(<App />);
-  const appComponent = wrapper.find('[data-test="component-app"]');
+  const wrapper = setup();
+  const appComponent = findByTestAttribute(wrapper, "component-app")
   expect(appComponent.length).toBe(1);
 }); 
 
 test("renders button", () => {
-
+  const wrapper = setup(); 
+  const button = findByTestAttribute(wrapper, "increment-button")
+  expect(button.length).toBe(1);
 });
 
 test("renders counter display", () => {
-
+  const wrapper = setup();
+  const counterDisplay = findByTestAttribute(wrapper, "counter-display")
+  expect(counterDisplay.length).toBe(1);
 });
 
 test("counter starts at 0", () => {
-
+  const wrapper = setup(); 
+  const count = findByTestAttribute(wrapper, "count").text(); 
+  expect(count).toBe("0");
 }); 
 
 test("clicking on button increments counter display", () => {
+// find the button, then click the button, find the display, and test that the number has been incremented
 
+const wrapper = setup(); 
+const button = findByTestAttribute(wrapper, "increment-button"); 
+button.simulate('click'); 
+const count = findByTestAttribute(wrapper, "count").text();
+expect(count).toBe("1"); 
 });
+
+test("renders decrement button", () => {
+  const wrapper = setup(); 
+  const button = findByTestAttribute(wrapper, "decrement-counter"); 
+  expect(button.length).toBe(1);
+}); 
+
+
+test("clicking on button decrements the counter and counter does not go below zero", () => {
+  const wrapper = setup(); 
+  const button = findByTestAttribute(wrapper, "decrement-counter"); 
+  button.simulate('click'); 
+  const count = findByTestAttribute(wrapper, "count").text(); 
+  expect(count).toBe("-1");
+}); 
+
+test("do not let the count go below 0", () => {
+  const wrapper = setup(); 
+  const count = findByTestAttribute(wrapper, "decrement-counter").get(0); 
+  expect(count).toBe(0);
+  count.simulate("click");
+  new Error("The count can not go below zero")
+});
+
+
 
 
